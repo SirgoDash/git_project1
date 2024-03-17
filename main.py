82f13271309e68e725964e17ffc278dee5c4954b
Sirgo <sirgod@yandex.ru>
2024-03-17 21:44:02 +0500

    cappuchino_0.1

--- main.py
+++ main.py
@@ -5,6 +5,57 @@ from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 import sys
 
 
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.con = sqlite3.connect("coffee.sqlite")
+        self.pushButton.clicked.connect(self.update_result)
+        self.tableWidget.itemChanged.connect(self.item_changed)
+        self.pushButton_2.clicked.connect(self.save_results)
+        self.pushButton_4.clicked.connect(self.show_base)
+        self.main_base = DBSample()
+        self.modified = {}
+        self.titles = None
+
+
+    def update_result(self):
+        cur = self.con.cursor()
+        result = cur.execute("SELECT * FROM coffee WHERE id=?",
+                             (item_id := self.spinBox.text(),)).fetchall()
+        self.tableWidget.setRowCount(len(result))
+        if not result:
+            self.statusBar().showMessage('Ничего не нашлось')
+            return
+        else:
+            self.statusBar().showMessage(f"Нашлась запись с id = {item_id}")
+        self.tableWidget.setColumnCount(len(result[0]))
+        self.titles = [description[0] for description in cur.description]
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+        self.modified = {}
+
+    def item_changed(self, item):
+        self.modified[self.titles[item.column()]] = item.text()
+
+    def save_results(self):
+        if self.modified:
+            cur = self.con.cursor()
+            que = "UPDATE coffee SET\n"
+            que += ", ".join([f"{key}='{self.modified.get(key)}'"
+                              for key in self.modified.keys()])
+            que += "WHERE id = ?"
+            print(que)
+            cur.execute(que, (self.spinBox.text(),))
+            self.con.commit()
+            self.modified.clear()
+
+    def show_base(self):
+        self.main_base.show()
+        self.close()
+
+
 class DBSample(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -29,6 +80,6 @@ class DBSample(QMainWindow):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = DBSample()
+    ex = MyWidget()
     ex.show()
     sys.exit(app.exec())
